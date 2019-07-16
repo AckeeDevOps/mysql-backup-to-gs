5646592db6f687af3980bca6dceb57c4ac93b234
Tomáš Hejátko <tomas.hejatko@gmail.com>
2019-07-16 12:25:19 +0200

    add force paramater for MySQL backups (allows backuping with broken references

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,12 +11,17 @@ if [ -z "$GS_URL" -o -z "$MYSQL_HOST" ]; then
     exit 1
 fi
 
+FORCE=""
+if [ ! -z "$MYSQL_DUMP_FORCE" ]; then
+  FORCE="--force"
+fi
+
 # verify gs config - ls bucket
 $backup_tool ls "gs://${GS_URL%%/*}" > /dev/null
 echo "Google storage bucket access verified."
 
 mkdir -p /tmp/backup/
 rm -rf -- /tmp/backup/* 
-mysqldump -u $MYSQL_USER -p'$MYSQL_PASSWORD' --all-databases --single-transaction -h "$MYSQL_HOST" -P "$MYSQL_PORT" --result-file=/tmp/backup/dump.sql --verbose && gzip /tmp/backup/dump.sql
+mysqldump -u $MYSQL_USER -p"$MYSQL_PASSWORD" --all-databases --single-transaction -h $MYSQL_HOST -P $MYSQL_PORT --result-file=/tmp/backup/dump.sql --verbose $FORCE && gzip /tmp/backup/dump.sql
 
 $backup_tool $backup_options /tmp/backup/ gs://$GS_URL/ 
